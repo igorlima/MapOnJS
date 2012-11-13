ebc1b20cd2b12a7e125b60770959f587f3b072dc
Igor Lima <igor.lima@evolut.io>
2012-11-12 23:57:55 -0200

    Um tanto de coisa

---
+++ server.js
@@ -0,0 +1,31 @@
+
+
+var express = require('express');
+var app = express();
+app.set('title', 'Meu Site');
+
+app.use(express.compress());
+app.get('/', function(req, res){
+  res.json({ user: 'tobi' });
+});
+
+app.get('/teste', function(req, res){
+  res.format({
+    text: function(){
+      res.send('hey');
+    },
+    
+    html: function(){
+      res.send('hey');
+    },
+    
+    json: function(){
+      res.send({ message: 'hey' });
+    }
+  });
+});
+
+app.use('/static', express.static(__dirname + '/'));
+
+
+app.listen(8080);
